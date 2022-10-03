eadc3798995ff2979cf9ed0757ad3308a3b578b3
therobby <40268355+therobby@users.noreply.github.com>
2022-10-03 22:26:35 +0200

    fix: random string without bigger letters

--- server/Helpsers/randomString.ts
+++ server/Helpsers/randomString.ts
@@ -1,6 +1,7 @@
 export default function randomString(length: number) {
-    const chars =
-        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // const chars =
+    //     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
     var result = "";
     for (var i = length; i > 0; --i) {
         result += chars[Math.floor(Math.random() * chars.length)];
