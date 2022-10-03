d131193a8237135f2817fed602d7dabc6e4c920d
therobby <40268355+therobby@users.noreply.github.com>
2022-10-03 22:33:07 +0200

    fix: random str with only lower letters

--- server/Helpsers/randomString.ts
+++ server/Helpsers/randomString.ts
@@ -1,7 +1,8 @@
 export default function randomString(length: number) {
     // const chars =
     //     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    const chars = "abcdefghijklmnopqrstuvwxyz";
+    // const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
     var result = "";
     for (var i = length; i > 0; --i) {
         result += chars[Math.floor(Math.random() * chars.length)];
